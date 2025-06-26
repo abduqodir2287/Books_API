12b8399ab52147da5a526a70c2982c9a44e325a1
abduqodir2287 <kadyrovk316@gmail.com>
2025-06-26 22:51:44 +0500

    Added nginx proxy server

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,3 @@ COPY requirements.txt .
 RUN pip install --upgrade pip && pip install -r requirements.txt
 
 COPY . .
-
-CMD ["gunicorn", "booksapi.wsgi:application", "--bind", "0.0.0.0:8000", "--workers=2"]
--- book_api/settings.py
+++ book_api/settings.py
@@ -20,11 +20,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
